1a1e29b9ea0e9366f42e9517716cc1de6e0f160a
cindilyy <lee.cindy720@gmail.com>
2023-04-11 03:22:41 -0700

    portal page

---
+++ script.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const image = document.querySelector('img');
+    const btnImage = document.querySelector('button img');
+    const moveImage = document.querySelector('.shooting-star');
+    const sections = document.querySelectorAll('section')
+    
+    let mode = 'dark';
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            banner.className = 'switch';
+            button.className = 'switch';
+            image.src = 'images/test9.png';
+            btnImage.src = 'images/moon.png';
+            moveImage.src= 'images/bird.png';
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            
+            mode = 'light';
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            button.removeAttribute('class');
+            image.src = 'images/test7.png';
+            btnImage.src = 'images/sun.png';
+            moveImage.src= 'images/star2.png';
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            mode = 'dark'
+        }
+    })
+})()
