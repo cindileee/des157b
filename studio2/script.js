54d501e159ba1cf4d75a72192119339bc96b6dfc
cindilyy <lee.cindy720@gmail.com>
2023-05-03 04:17:12 -0700

    check yourself

--- studio2/script.js
+++ studio2/script.js
@@ -1,12 +1,12 @@
 (function(){
     'use strict';
 
-    // const scrollContainer = document.querySelector("main");
+    const scrollContainer = document.querySelector("main");
 
-    // scrollContainer.addEventListener("wheel", function(event) {
-    //     event.preventDefault();
-    //     scrollContainer.scrollLeft += event.deltaY;
-    // });
+    scrollContainer.addEventListener("wheel", function(event) {
+        event.preventDefault();
+        scrollContainer.scrollLeft += event.deltaY;
+    });
 
 
 
@@ -50,7 +50,8 @@
         let html = '';
         data.forEach( function(eachEntry){
            
-            html += '<div id="movie">';
+            // html += '<div id="movie">';
+            html += '<section id="movie">';
             html += `<img src ="images/${eachEntry.img}" class="titleimg">`;
             html += '<div id="info" >';
             html += `<h2>${eachEntry.title}</h2>`;
@@ -59,18 +60,21 @@
             html += '<div id="outside">';
             html += '</div>';
             html += '</div>';
-            html += '</div>';
+            // html += '</div>';
+            html += '</section>';
             
-            const imdbNumber = eachEntry.imdb_number;
-            // console.log(imdbNumber);
-            getTitleInfo(imdbNumber);
+            // const imdbNumber = eachEntry.imdb_number;
+            // // console.log(imdbNumber);
+            // getTitleInfo(imdbNumber);
             
             
 
             
         } );
         
+        
         return html;
+        
 
         
         
@@ -85,30 +89,37 @@
 
     
 
-    async function getTitleInfo(title){
-        const url = `http://www.omdbapi.com/?i=${title}&plot=full&apikey=b6a9b242`;
-        const titleData = await fetch(url);
-        const data = await titleData.json();
-        // console.log(data);
-        document.querySelector("#outside").innerHTML = outputHTML1(data);
-    }
+    // async function getTitleInfo(title){
+    //     const url = `http://www.omdbapi.com/?i=${title}&plot=full&apikey=b6a9b242`;
+    //     const titleData = await fetch(url);
+    //     const data = await titleData.json();
+    //     // console.log(data);
+    //     // document.querySelector("#outside").innerHTML = outputHTML1(data);
+    //     document.querySelector("#container1").innerHTML = outputHTML1(data);
+    // }
     
-    function outputHTML1(data){
+    // function outputHTML1(data){
 
         
-        let html1 = '';
+    //     let html1 = '';
        
-        html1 += `<h2>${data.Title}</h2>`;
-        html1 += `<p>Released: ${data.Released} </p>`;
-        html1 += `<p>Actors: ${data.Actors}</p>`;
-        html1 += `<p>${data.Plot}</p>`;
+    //     html1 += `<h2>${data.Title}</h2>`;
+    //     html1 += `<p>Released: ${data.Released} </p>`;
+    //     html1 += `<p>Actors: ${data.Actors}</p>`;
+    //     html1 += `<p>${data.Plot}</p>`;
         
-        console.log(data.Title);
-        console.log(data.Released);
-        return html1;
+    //     console.log(data.Title);
+    //     console.log(data.Released);
+    //     return html1;
         
         
-    }
+    // }
+    
+
+
+
+
+
     
 
     
