3a14e32621090463812af54b11731018e23da3c5
cindilyy <lee.cindy720@gmail.com>
2023-05-04 02:05:34 -0700

    check yourself

--- studio2/script.js
+++ studio2/script.js
@@ -1,6 +1,7 @@
 (function(){
     'use strict';
 
+    //to scroll horizontally
     const scrollContainer = document.querySelector("main");
 
     scrollContainer.addEventListener("wheel", function(event) {
@@ -10,62 +11,40 @@
 
 
 
-    
-
-
-
+    //get my own json data
     async function getData(){
         const watched = await fetch('data/movies.json');
         const data = await watched.json();
         const values = Object.values(data);
         console.log(values);
-        // document.querySelector('#showdata').innerHTML = outputHTML(values);
-        // document.querySelector('#container').innerHTML = outputHTML(values);
-        // document.querySelector('#titleimg').innerHTML = outputHTML(values);
         document.querySelector('#container').innerHTML = outputHTML(values);
 
-
-        // document.querySelector('#info').innerHTML = outputHTML2(values);
-
-        // document.querySelector(".titleimg").addEventListener("mouseover", function(event){
-        
-        //     event.preventDefault();
-        //     info.className = "showing";
-            
-            
-        // });
-    
-        // document.querySelector(".titleimg").addEventListener("mouseout", function(event){
-            
-        //     event.preventDefault();
-        //     info.className = "hidden";
-        //     // info.className = "showing";
-            
-            
-            
-        // });
     }
     
+    //for each movie, it will show the name, watch date, and my rating
     function outputHTML(data){
         let html = '';
-        data.forEach( function(eachEntry){
+        data.forEach( function(eachEntry, index){
            
-            // html += '<div id="movie">';
             html += '<section id="movie">';
-            html += `<img src ="images/${eachEntry.img}" class="titleimg">`;
-            html += '<div id="info" >';
+            html += `<img src ="images/${eachEntry.img}" class="titleimg" >`;
+            html += '<div class="info">';
             html += `<h2>${eachEntry.title}</h2>`;
-            html += `<p>Watch Date: ${eachEntry.date}</p>`;
-            html += `<p>Rating: ${eachEntry.rating}/5</p>`;
+
             html += '<div id="outside">';
             html += '</div>';
-            html += '</div>';
-            // html += '</div>';
-            html += '</section>';
+
+            html += `<p id="me1">Watch Date: ${eachEntry.date}</p>`;
+            html += `<p id="me2">Cindy's Rating: ${eachEntry.rating}/5</p>`;
             
-            // const imdbNumber = eachEntry.imdb_number;
-            // // console.log(imdbNumber);
-            // getTitleInfo(imdbNumber);
+
+            html += '</div>';
+            html += '</section>';
+
+
+
+            const imdbNumber = eachEntry.imdb_number;
+            getTitleInfo(imdbNumber,index);
             
             
 
@@ -75,55 +54,44 @@
         
         return html;
         
-
-        
         
     } 
 
+
+    //fetch outside data from IMDB
+    async function getTitleInfo(title,index){
+        const url = `http://www.omdbapi.com/?i=${title}&plot=full&apikey=b6a9b242`;
+        const titleData = await fetch(url);
+        const data = await titleData.json();
+        
+        // Update querySelector to select the correct element based on the index
+        document.querySelectorAll('#outside')[index].innerHTML = outputHTML1(data);
+
+        
+    }
+    
+    //displays release date, director name, and summary of the plot
+    function outputHTML1(data){
+
+        
+        let html1 = '';
+        html1 += `<p id="outsidedetails">Released: ${data.Released} </p>`;
+        html1 += `<p id="outsidedetails">Director: ${data.Director} </p>`;
+        html1 += `<p id="plot">${data.Plot}</p>`;
+        
+        console.log(data.Title);
+        console.log(data.Released);
+        return html1;
+
+
+        
+        
+        
+    }
     
   
     
     getData();
-
-
-
-    
-
-    // async function getTitleInfo(title){
-    //     const url = `http://www.omdbapi.com/?i=${title}&plot=full&apikey=b6a9b242`;
-    //     const titleData = await fetch(url);
-    //     const data = await titleData.json();
-    //     // console.log(data);
-    //     // document.querySelector("#outside").innerHTML = outputHTML1(data);
-    //     document.querySelector("#container1").innerHTML = outputHTML1(data);
-    // }
-    
-    // function outputHTML1(data){
-
-        
-    //     let html1 = '';
-       
-    //     html1 += `<h2>${data.Title}</h2>`;
-    //     html1 += `<p>Released: ${data.Released} </p>`;
-    //     html1 += `<p>Actors: ${data.Actors}</p>`;
-    //     html1 += `<p>${data.Plot}</p>`;
-        
-    //     console.log(data.Title);
-    //     console.log(data.Released);
-    //     return html1;
-        
-        
-    // }
-    
-
-
-
-
-
-    
-
-    
-    
 
 
     
